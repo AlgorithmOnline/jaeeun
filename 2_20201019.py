21e2373f8dffa864be0267ce934607c112db6e00
rha3122@naver.com <rha3122@naver.com>
2020-10-19 17:59:43 +0900

    백준 1495기타리스트 ,백준 10282 해킹

--- 2_20201019.py
+++ 2_20201019.py
@@ -1,12 +1,33 @@
-x= input()
-y = input()
-
-dp = [[0] * (len(y)+1) for _ in ragne(len(x)+1)]
-
-for i in range(1, len(x)+1):
-	for j in range(1, len(y)+1):
-    	if x[i-1] == y[j-1]:
-        	dp[i][j] = dp[i-1][j-1] +1
-        else:
-        	dp[i][j] = max(dp[i-1][j-1] , dp[i-1][j])
-print(dp[len(x)][len(y)])
+from heapq import heappush, heappop
+import sys
+#input = sys.stdin.readline
+inf = 100000000
+def dijkstra(start):
+    heap = []
+    heappush(heap, [0, start])
+    dp = [inf for i in range(n + 1)]
+    dp[start] = 0
+    while heap:
+        we, nu = heappop(heap)
+        for ne, nw in s[nu]:
+            wei = we + nw
+            if dp[ne] > wei:
+                dp[ne] = wei
+                heappush(heap, [wei, ne])
+    return dp
+t = int(input())
+for _ in range(t):
+    n, d, start = map(int, input().split())
+    s = [[] for i in range(n + 1)]
+    for i in range(d):
+        a, b, c = map(int, input().split())
+        s[b].append([a, c])
+    dp = dijkstra(start)
+    max_dp = 0
+    cnt = 0
+    for i in dp:
+        if i != inf:
+            if max_dp < i:
+                max_dp = i
+            cnt += 1
+    print(cnt, max_dp)
