48153d30d1caf6a9b67aedbf800ad065fb5e113c
KilJaeeun <rha3122@naver.com>
2020-11-23 23:56:57 +0900

    프로그래머스 수밧수박수박수? 3진법 뒤집기

---
+++ 1_20201123.py
@@ -0,0 +1,4 @@
+def solution(n):
+    a="수박"*10000
+    answer = ''
+    return a[:n]
---
+++ 2_20201123.py
@@ -0,0 +1,16 @@
+NOTATION = '0123456789ABCDEF' 
+def numeral_system(number, base): 
+    q, r = divmod(number, base) 
+    n = NOTATION[r] 
+    return numeral_system(q, base) + n if q else n
+
+
+def solution(n):
+    answer=0
+    three_num = str(int(str(numeral_system(n, 3))[::-1]))
+    for i in range(len(three_num)):
+      #  print(int(three_num[i]),(len(three_num)-i-1))
+        answer+=int(three_num[i])*3**(len(three_num)-i-1)
+    
+    
+    return answer
