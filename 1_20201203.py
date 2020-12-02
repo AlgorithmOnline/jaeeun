bfa35201af9eb497533cf5853d02ab1b07793a78
KilJaeeun <rha3122@naver.com>
2020-12-02 16:14:32 +0900

    백준 15650번 N과 M (2),백준 15651번 (3)

---
+++ 1_20201203.py
@@ -0,0 +1,16 @@
+N, M = map(int, input().split())
+maps=list(range(1, N+1))
+def dfs(maps,visit ):
+    if len(visit)==M:
+        print(*visit)
+        return
+    elif len(maps)==0:
+        return 
+    else:
+        for i in range(0, len(maps)):
+            visit2= visit[:]
+            visit2.append(maps[i])
+            dfs(maps[:], visit2)
+
+for i in range(0, len(maps)):
+    dfs(maps[:], [maps[i]])
